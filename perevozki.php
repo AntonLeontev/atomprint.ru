3b3672cbead9959038f15438a66a6b7cd11d4678
Aner <aner-anton@ya.ru>
2020-05-09 23:10:50 +0300

    Добавлена проверка на спам по тайтлу страницы. Сделана верстка ПК версии страницы перевозок

--- perevozki.php
+++ perevozki.php
@@ -12,6 +12,7 @@
   <link rel="icon" href="https://xn--80aqlfchhmg.xn--p1ai/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="/assets/css/reset.css">
   <link rel="stylesheet" href="/assets/css/perevozki.css">
+  <link rel="stylesheet" href="/assets/css/blocks/pop-up_map.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script type="text/javascript" src="/assets/js/scripts_body.js"></script>
   <script type="text/javascript" src="/assets/js/jquery.flexisel.js"></script>
@@ -45,6 +46,10 @@
   <header class="header">
     <div class="header_wrap">
       <div class="logo"><span class="logo_red">АТОМ</span>ПРИНТ</div>
+      <div class="ati_small">
+        <iframe scrolling="no" width="200" height="70" frameborder="0" src="https://ati.su/widgets/badge/?id=841611&type=gray">
+        </iframe>
+      </div>
       <adress class="adress">г. Екатеринбург пер. Автоматики 1 оф.007</adress>
       <div class="contacts">
         <div class="tel contact">+7 343 300-92-33</div>
@@ -55,13 +60,13 @@
   <main class="main">
     <div class="first_screen">
       <h1 class="h1">Перевозки грузов по Екатеринбургу и России</h1>
-      <h2 class="h2">Грузы от 100 до 2500 кг</h2>
-      <table class="price">
+      <h2 class="h2">Заказать газель</h2>
+      <table class="table price">
         <h3 class="h3">Стоимость</h3>
         <tr class="table_row">
           <td class="table_col"></td>
           <td class="table_title table_col">За км</td>
-          <td class="table_title table_col">Почасово (минимум 3ч)</td>
+          <td class="table_title table_col">Почасово (минимум&nbsp;3ч)</td>
         </tr>
         <tr class="table_row">
           <td class="table_title table_col">до 1,5 т</td>
@@ -75,15 +80,31 @@
         </tr>
       </table>
 
-      <form action="#" class="form">
-        <span class="form_title">Оставьте заявку и мы перезвоним</span>
-        <input class="input_text" type="text" placeholder="Имя">
-        <input class="phone_inp" type="tel" placeholder="Телефон">
-        <label class="form_label"><input type="checkbox">Даю согласие на обработку персональных данных</input></label>
-        <input class="input_submit" type="submit" value="Отправить">
-      </form>
+      <table class="table size">
+        <h3 class="h3">Вместимость кузова</h3>
+        <tr class="table_row">
+          <td class="table_title table_col">Длина</td>
+          <td class="table_title table_col">Ширина</td>
+          <td class="table_title table_col">Высота</td>
+          <td class="table_title table_col">Количество палет</td>
+        </tr>
+        <tr class="table_row">
+          <td class="table_col">4 м</td>
+          <td class="table_col">2 м</td>
+          <td class="table_col">1.9 м</td>
+          <td class="table_col">до 8 палет</td>
+        </tr>        
+      </table>
+
+      
     </div>
   </main>
+
+
+  <?php 
+    include_once __DIR__ . '/assets/php/footer.php';
+    include_once __DIR__ . '/assets/php/map-pop-up.php';
+  ?>
 
      
 
--- thanks_page.php
+++ thanks_page.php
@@ -18,60 +18,64 @@
 
 <?php
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (!empty($_POST['phone'])){
-	  if (isset($_POST['name'])) {
-	    if (!empty($_POST['name'])){
-	  $name = strip_tags(mb_strtoupper($_POST['name']));
-	  $nameFieldset = "Имя: ";
-	  	}
+		if ($_POST['page-title'] != 'Нет данных') {
+			if (!empty($_POST['phone'])){
+			  if (isset($_POST['name'])) {
+			    if (!empty($_POST['name'])){
+			  	$name = strip_tags(mb_strtoupper($_POST['name']));
+			  	$nameFieldset = "Имя: ";
+			  	}
+				}
+
+				if (isset($_POST['phone'])) {
+				  if (!empty($_POST['phone'])){
+				  $phone = strip_tags($_POST['phone']);
+				  $phoneFieldset = "Телефон: ";
+				  }
+				}
+				if (isset($_POST['form-name'])) {
+				  if (!empty($_POST['form-name'])){
+				  $formName = strip_tags($_POST['form-name']);
+				  $formNameFieldset = "Форма: ";
+				  }
+				}
+				if (isset($_POST['page-title'])) {
+				  if (!empty($_POST['page-title'])){
+				  $pageTitle = strip_tags($_POST['page-title']);
+				  $pageTitleFieldset = "Страница: ";
+				  }
+				}
+				$token = "961313657:AAGAMoIvveEHv3GiEC_Sed4uXByUPvLZXiA";
+				$chat_id = "-322308753";
+				$arr = array(
+				  $nameFieldset => $name,
+				  $phoneFieldset => $phone,
+					$pageTitleFieldset => $pageTitle,
+					$formNameFieldset => $formName,
+				);
+				foreach($arr as $key => $value) {
+				  $txt .= "<b>".$key."</b> ".$value."%0A";
+				};
+				$sendMailAner = mail('aner-anton@ya.ru', 'НОВАЯ ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
+				$sendMailAtom = mail('atomprint@yandex.ru', 'НОВАЯ, ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
+				$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+				$message = 'Принято!';
+				if ($sendToTelegram) {
+
+				  $message = "Принято! Мы скоро позвоним по номеру <br>$phone!";
+				    //return true;
+				} else {
+			  $message = 'Ошибка. Сообщение не отправлено! Попробуйте позвонить нам';
+				}
+			} else {
+	  		$message = 'Ошибка. Введите номер телефона еще раз';
+			}
+		} else {
+			$sendMailAner = mail('aner-anton@ya.ru', 'СПАМ-ФИЛЬТР АТОМПРИНТ', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
 		}
-
-	if (isset($_POST['phone'])) {
-	  if (!empty($_POST['phone'])){
-	  $phone = strip_tags($_POST['phone']);
-	  $phoneFieldset = "Телефон: ";
-	  }
-	}
-	if (isset($_POST['form-name'])) {
-	  if (!empty($_POST['form-name'])){
-	  $formName = strip_tags($_POST['form-name']);
-	  $formNameFieldset = "Форма: ";
-	  }
-	}
-	if (isset($_POST['page-title'])) {
-	  if (!empty($_POST['page-title'])){
-	  $pageTitle = strip_tags($_POST['page-title']);
-	  $pageTitleFieldset = "Страница: ";
-	  }
-	}
-	$token = "961313657:AAGAMoIvveEHv3GiEC_Sed4uXByUPvLZXiA";
-	$chat_id = "-322308753";
-	$arr = array(
-	  $nameFieldset => $name,
-	  $phoneFieldset => $phone,
-		$pageTitleFieldset => $pageTitle,
-		$formNameFieldset => $formName,
-	);
-	foreach($arr as $key => $value) {
-	  $txt .= "<b>".$key."</b> ".$value."%0A";
-	};
-	$sendMailAner = mail('aner-anton@ya.ru', 'НОВАЯ ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
-	$sendMailAtom = mail('atomprint@yandex.ru', 'НОВАЯ, ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
-	$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-	$message = 'Принято!';
-	if ($sendToTelegram) {
-
-	  $message = "Принято! Мы скоро позвоним по номеру <br>$phone!";
-	    //return true;
 	} else {
-	  $message = 'Ошибка. Сообщение не отправлено! Попробуйте позвонить нам';
-	}
-} else {
-	  $message = 'Ошибка. Введите номер телефона еще раз';
-	}
-} else {
 	header ("Location: /");
-}
+	}
 
 ?>
 
