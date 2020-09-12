2850c8c2d2af18604ec24c7ec2a11147d9161f0a
Anton <aner-anton@ya.ru>
2020-09-12 23:55:34 +0300

    Добавлена отправка форм в битрикс24, исправлены мелкие ошибки

--- thanks_page.php
+++ thanks_page.php
@@ -8,6 +8,12 @@
 		ini_set('display_startup_errors', 0);
 		error_reporting(0);
 		include_once 'assets/php/head.php';
+
+		function mb_ucfirst($string) {
+      $first = mb_substr($string, 0, 1);
+      $else = mb_substr($string, 1);
+      return mb_strtoupper($first).mb_strtolower($else);
+    }
 	 ?>
 </head>
 <body>
@@ -18,10 +24,13 @@
 
 <?php
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (!empty($_POST['phone'])){
+		
+		// Подготавливаем данные к отправке
+
+		if (!empty($_POST['phone'])){ 
 		  if (isset($_POST['name'])) {
 		    if (!empty($_POST['name'])){
-		  	$name = strip_tags(mb_strtoupper($_POST['name']));
+		  	$name = strip_tags(mb_ucfirst($_POST['name']));
 		  	$nameFieldset = "Имя: ";
 		  	}
 			}
@@ -47,6 +56,7 @@
 			  }
 			}
 
+			// для отправки в телеграм
 			$token = "961313657:AAGAMoIvveEHv3GiEC_Sed4uXByUPvLZXiA";
 			$chat_id = "-322308753";
 			$arr = array(
@@ -59,18 +69,61 @@
 			  $txt .= "<b>".$key."</b> ".$value."%0A";
 			};
 
-			if ($_POST['page-title'] != 'Нет данных') {
-				$sendMailAner = mail('aner-anton@ya.ru', 'НОВАЯ ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
+			//для отправки в битрикс
+			// формируем URL в переменной $queryUrl
+			$queryUrl = 'https://b24-kc1wpe.bitrix24.ru/rest/1/ly5bjer0c86qo39q/crm.lead.add.json';
+
+			// формируем параметры для создания лида в переменной $queryData
+			$queryData = http_build_query(array(
+			  'fields' => array(
+			    'TITLE' => "Заявка с сайта",
+			    'NAME' => $name,
+			    'COMMENTS' => "Заявка!\n\r${txt}",
+			  	'PHONE' => Array(
+				    "n0" => Array(
+				        "VALUE" => $phone,
+				        "VALUE_TYPE" => "WORK",
+				    ),
+					),
+					"SOURCE_ID" => "Сайт",
+					'WEB' => $pageTitle,
+
+
+			  ),
+			  'params' => array("REGISTER_SONET_EVENT" => "Y")
+			));
+
+			if ($_POST['page-title'] != 'Нет данных') { //проверка на спам
+
+				// отправляем письма
+				// $sendMailAner = mail('aner-anton@ya.ru', 'НОВАЯ ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
 				$sendMailAtom = mail('atomprint@yandex.ru', 'НОВАЯ, ЗАЯВКА!', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
+
+				// отправляем в телеграм
 				$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-				$message = 'Принято!';
-				if ($sendToTelegram) {
+
+				// отправляем в битрикс 
+				$curl = curl_init();
+				curl_setopt_array($curl, array(
+				  CURLOPT_SSL_VERIFYPEER => 0,
+				  CURLOPT_POST => 1,
+				  CURLOPT_HEADER => 0,
+				  CURLOPT_RETURNTRANSFER => 1,
+				  CURLOPT_URL => $queryUrl,
+				  CURLOPT_POSTFIELDS => $queryData,
+				));
+				$result = curl_exec($curl);
+				curl_close($curl);
+				$result = json_decode($result, 1);
+				if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description']."<br/>";
+
+				if ($sendToTelegram || $sendMailAtom) {
 				  $message = "Принято! Мы скоро позвоним по номеру <br>$phone!";
-				    //return true;
 				} else {
 			  $message = 'Ошибка. Сообщение не отправлено! Попробуйте позвонить нам';
 				}
-			} else {
+			} else { 
+			// если спам
 				$sendMailAner = mail('aner-anton@ya.ru', 'СПАМ-ФИЛЬТР АТОМПРИНТ', "\r\n\r\nИмя: $name; \r\nТелефон: $phone \r\n\r\nЗвони скорее!!!\r\n\r\nСтраница: $pageTitle\r\nФорма: $formName");
 			}
 		} else {
